337537d7d50adb08a03c9a371eb9ffcc82366ae9
Garegin845 <139650368+Garegin845@users.noreply.github.com>
2023-08-02 10:11:03 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,21 +4,81 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+       <div style ={{display:"flex"}}>
+        <div style = {{border:"1px solid",width:"120px",height:"30px"}}><p style={{marginRight:"310px",marginTop:"5px"}}>John</p></div>
+        <div style = {{border:"1px solid",width:"120px",height:"30px"}}><p style={{marginRight:"310px",marginTop:"5px"}}>Doe</p></div>
+        <div style = {{border:"1px solid ",width:"47px",height:"30px",backgroundColor:"rgb(172,214,199)"}}><p style={{marginRight:"310px",marginTop:"5px"}}>47</p></div>
+
     </div>
+    
+     <div style ={{display:"flex"}}>
+        <div style = {{border:"1px solid",width:"120px",height:"30px"}}><p style={{marginRight:"310px",marginTop:"5px"}}>Jane</p></div>
+        <div style = {{border:"1px solid",width:"120px",height:"30px"}}><p style={{marginRight:"310px",marginTop:"5px"}}>Smith</p></div>
+        <div style = {{border:"1px solid ",width:"28px",height:"30px",backgroundColor:"rgb(203,185,220)"}}><p style={{marginRight:"310px",marginTop:"5px"}}>28</p></div>
+</div>
+
+
+<div style ={{display:"flex"}}>
+        <div style = {{border:"1px solid",width:"120px",height:"30px"}}><p style={{marginRight:"310px",marginTop:"5px"}}>Alice</p></div>
+        <div style = {{border:"1px solid",width:"120px",height:"30px"}}><p style={{marginRight:"310px",marginTop:"5px"}}>Johnson</p></div>
+        <div style = {{border:"1px solid ",width:"33",height:"30px",backgroundColor:"rgb(225,253,241)"}}><p style={{marginRight:"310px",marginTop:"5px"}}>33</p></div>
+</div>
+
+
+ <div style ={{display:"flex"}}>
+        <div style = {{border:"1px solid",width:"120px",height:"30px"}}><p style={{marginRight:"310px",marginTop:"5px"}}>Bob</p></div>
+        <div style = {{border:"1px solid",width:"120px",height:"30px"}}><p style={{marginRight:"310px",marginTop:"5px"}}>Brown</p></div>
+        <div style = {{border:"1px solid ",width:"65px",height:"30px",backgroundColor:"rgb(248,192,255)"}}><p style={{marginRight:"310px",marginTop:"5px"}}>65</p></div>
+</div>
+
+
+
+ <div style ={{display:"flex"}}>
+        <div style = {{border:"1px solid",width:"120px",height:"30px"}}><p style={{marginRight:"310px",marginTop:"5px"}}>Charles</p></div>
+        <div style = {{border:"1px solid",width:"120px",height:"30px"}}><p style={{marginRight:"310px",marginTop:"5px"}}>Davis</p></div>
+        <div style = {{border:"1px solid ",width:"50px",height:"30px",backgroundColor:"rgb(214,234,255)"}}><p style={{marginRight:"310px",marginTop:"5px"}}>50</p></div>
+</div>
+
+
+
+<div style ={{display:"flex"}}>
+        <div style = {{border:"1px solid",width:"120px",height:"30px"}}><p style={{marginRight:"310px",marginTop:"5px"}}>Eve</p></div>
+        <div style = {{border:"1px solid",width:"120px",height:"30px"}}><p style={{marginRight:"310px",marginTop:"5px"}}>White</p></div>
+        <div style = {{border:"1px solid ",width:"70px",height:"30px",backgroundColor:"rgb(234,250,210)"}}><p style={{marginRight:"310px",marginTop:"5px"}}>70</p></div>
+</div>
+
+
+
+<div style ={{display:"flex"}}>
+        <div style = {{border:"1px solid",width:"120px",height:"30px"}}><p style={{marginRight:"310px",marginTop:"5px"}}>Mollory</p></div>
+        <div style = {{border:"1px solid",width:"120px",height:"30px"}}><p style={{marginRight:"310px",marginTop:"5px"}}>Harris</p></div>
+        <div style = {{border:"1px solid ",width:"38px",height:"30px",backgroundColor:"rgb(255,244,172)"}}><p style={{marginRight:"310px",marginTop:"5px"}}>38</p></div>
+</div>
+
+
+
+<div style ={{display:"flex"}}>
+        <div style = {{border:"1px solid",width:"120px",height:"30px"}}><p style={{marginRight:"310px",marginTop:"5px"}}>Sam</p></div>
+        <div style = {{border:"1px solid",width:"120px",height:"30px"}}><p style={{marginRight:"310px",marginTop:"5px"}}>Clark</p></div>
+        <div style = {{border:"1px solid ",width:"80px",height:"30px",backgroundColor:"rgb(190,200,253)"}}><p style={{marginRight:"310px",marginTop:"5px"}}>80</p></div>
+</div>
+
+
+
+<div style ={{display:"flex"}}>
+        <div style = {{border:"1px solid",width:"120px",height:"30px"}}><p style={{marginRight:"310px",marginTop:"5px"}}>Trudy</p></div>
+        <div style = {{border:"1px solid",width:"120px",height:"30px"}}><p style={{marginRight:"310px",marginTop:"5px"}}>Thompson</p></div>
+        <div style = {{border:"1px solid ",width:"15px",height:"30px",backgroundColor:"rgb(233,222,171)"}}><p style={{marginRight:"310px",marginTop:"5px"}}>15</p></div>
+</div>
+
+
+
+<div style ={{display:"flex"}}>
+        <div style = {{border:"1px solid",width:"120px",height:"30px"}}><p style={{marginRight:"310px",marginTop:"5px"}}>Oscar</p></div>
+        <div style = {{border:"1px solid",width:"120px",height:"30px"}}><p style={{marginRight:"310px",marginTop:"5px"}}>Anderson</p></div>
+        <div style = {{border:"1px solid ",width:"95px",height:"30px",backgroundColor:"rgb(199,204,251)"}}><p style={{marginRight:"310px",marginTop:"5px"}}>95</p></div>
+</div></div>
   );
 }
 
